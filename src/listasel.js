64cb8e348fccb88b84d5e3e71006f3ce1efdfd06
Pallo Zoltan <pallozoli@yaho.com>
2020-03-23 14:30:40 +0200

    commit 5

--- src/listasel.js
+++ src/listasel.js
@@ -3,13 +3,17 @@ import "./listasel.css";
 class ListaSel extends Component {
   render() {
     const { eticheta, nume, lista } = this.props;
-    const optiuni = lista.map(item => <option>{item}</option>);
+    let listaVal = lista.map(item => (
+      <option value={item.id} key={item.id}>
+        {item.nume}
+      </option>
+    ));
     return (
       <div className="listasel form-group">
         <label htmlFor={nume}>{eticheta}</label>
         <select className="form-control" id={nume}>
           <option>Selectează județul</option>
-          {optiuni}
+          {listaVal}
         </select>
       </div>
     );
