c946fb8fcf0df69d56ab9e60ec34057b50c89d42
Pallo Zoltan <pallozoli@yaho.com>
2020-03-23 18:42:41 +0200

    commit 6

--- src/compar.js
+++ src/compar.js
@@ -5,18 +5,46 @@ class Compar extends Component {
       votantiRural = 14987;
     let width1 = (votantiUrban / (votantiUrban + votantiRural)) * 100; //  procente
     let width2 = (votantiRural / (votantiUrban + votantiRural)) * 100; //  procente
-    const div1 = `<div style={width: ${width1}%, height: "30px", lineHeight: "30px", 
-    textAlign: "center", display: "inline-block", float: "left", 
-    color: "white", backgroundColor: "#0288D1"}>Urban</div>`;
+    const div1 = (
+      <div
+        style={{
+          width: `${width1}%`,
+          height: "30px",
+          lineHeight: "30px",
+          textAlign: "center",
+          display: "inline-block",
+          float: "left",
+          color: "white",
+          backgroundColor: "#0288D1"
+        }}
+      >
+        Urban
+      </div>
+    );
 
-    const div2 = `<div style={width: ${width2}%, height: "30px", lineHeight: "30px", 
-    textAlign: "center", display: "inline-block", float: "left", 
-    color: "white", backgroundColor: "#BF360C"}>Rural</div>`;
+    const div2 = (
+      <div
+        style={{
+          width: `${width2}%`,
+          height: "30px",
+          lineHeight: "30px",
+          textAlign: "center",
+          display: "inline-block",
+          float: "left",
+          color: "white",
+          backgroundColor: "#BF360C"
+        }}
+      >
+        Rural
+      </div>
+    );
+
     const stil = { width: "1000px", margin: "0 auto" };
 
     return (
       <div style={stil}>
-        ${div1}${div2}
+        {div1}
+        {div2}
       </div>
     );
   }
